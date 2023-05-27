abd40a1179cbf4ff658c97604d11646b9b06126a
Abhilash <vunnamabhilash17072002@gmail.com>
2023-05-27 11:44:34 +0530

    Added a new function called playRound

--- myScript.js
+++ myScript.js
@@ -2,6 +2,35 @@ function getComputerChoice(){
     let computerChoice=["rock", "paper", "scissors"]
     const randomIndex=Math.floor(Math.random()*computerChoice.length)
     return computerChoice[randomIndex]
-
 }
 
+function playRound(playerSelection, computerSelection){
+    const playerSelections = playerSelection.toLowerCase()
+    if (playerSelections == computerSelection){
+        return "its a tie";
+    }
+    else if(playerSelections =="rock"&& computerSelection =="paper"){
+        return "You Lose! Paper beats Rock"
+
+    }
+    else if(playerSelections =="rock"&& computerSelection =="scissors"){
+        return "You Win! Rock beats Scissors"
+    }
+    else if(playerSelections=="paper" && computerSelection =="rock"){
+        return "You Win! Paper beats Rock" 
+    }
+    else if(playerSelections=="paper" && computerSelection =="scissors"){
+        return "You Lose! Scissors beats Paper"
+    }
+    else if(playerSelections=="scissors" && computerSelection =="rock"){
+        return "You Lose! rock beats scissors"
+    }
+    else if (playerSelections=="scissors" && computerSelection =="paper"){
+        return "You Win! scissors beats paper"
+
+    }
+    else{
+        return "something went wrong"
+    }
+
+}
