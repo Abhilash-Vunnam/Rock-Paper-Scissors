912dcffac65a66dd8aba09ea9906e52fa5263e93
Abhilash <vunnamabhilash17072002@gmail.com>
2023-06-07 11:36:25 +0530

    fixes the bugs in the functions

--- myScript.js
+++ myScript.js
@@ -3,81 +3,51 @@ function getComputerChoice(){
     const randomIndex=Math.floor(Math.random()*computerChoice.length);
     return computerChoice[randomIndex];
 }
-
+const computerSelection=getComputerChoice()
+console.log(computerSelection);
 
 function playRound(playerSelection, computerSelection) {
-    const playerSelections = playerSelection.toLowerCase();
-    const computerSelections = computerSelection.toLowerCase();
-  
-    const results = {
-      rock: {
-        beats: "scissors",
-        loses: "paper"
-      },
-      scissors: {
-        beats: "paper",
-        loses: "rock"
-      },
-      paper: {
-        beats: "rock",
-        loses: "scissors"
-      }
-    };
-  
-    if (playerSelections === computerSelections) {
-        score=-1
-      return "It's a tie";
-    } else {
-      const playerMove = results[playerSelections];
-      const computerMove = results[computerSelections];
-      if(playerSelections==computerMove.loses){
-        score=1
-        return `you win! ${playerSelections} beats ${computerSelections}`;
-      }
-      else{
-        score=0
-        return `you lose! ${computerSelections} beats ${playerSelections} `;
-      }
-      
-      
-    }
-  }
- 
+  const playerSelections = playerSelection.toLowerCase();
+  const computerSelections = computerSelection.toLowerCase();
  
 
- function game(){
-    let playerScore=0
-    let computerScore=0
-    for(let i=0;i<5;i++){
-        const playerSelection="Rock"
-        const computerSelection=getComputerChoice();
-        result=playRound(playerSelection, computerSelection)
-        if(score==1){
-            playerScore++;
-        }
-        else if(score==0){
-            computerScore++;
-        }
-        else if(score==-1){
 
-            continue;
-        }
-
-        
-
-        
+  const results = {
+    rock: {
+      beats: "scissors",
+      loses: "paper"
+    },
+    scissors: {
+      beats: "paper",
+      loses: "rock"
+    },
+    paper: {
+      beats: "rock",
+      loses: "scissors"
     }
-    if (playerScore>computerScore){
-        console.log( "you won!")
-    }
-    else if(playerScore<computerScore){
-        console.log ("computer won!")
+  };
+
+  if (playerSelections === computerSelections) {
+      score=-1
+    return "It's a tie";
+  } else {
+    const playerMove = results[playerSelections];
+    const computerMove = results[computerSelections];
+    if(playerSelections==computerMove.loses){
+      score=1
+      return `you win! ${playerSelections} beats ${computerSelections}`;
     }
     else{
-        console.log ("Tie")
+      score=0
+      return `you lose! ${computerSelections} beats ${playerSelections} `;
     }
     
+    
+  }
+}
 
- }
- 
- game()
+const playerSelection="rock"
+const game=playRound(playerSelection, computerSelection)
+console.log(game)
+
+
