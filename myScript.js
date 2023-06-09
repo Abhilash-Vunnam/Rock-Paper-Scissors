c7fb624ae582e970490fbc805eac65a0f2a36248
Abhilash <vunnamabhilash17072002@gmail.com>
2023-06-09 19:44:04 +0530

    added the game function

--- myScript.js
+++ myScript.js
@@ -7,7 +7,6 @@ function getComputerChoice(){
   
 }
 const computerSelection = getComputerChoice();
-console.log(computerSelection)
 
 // a function that plays single round of game
 function playRound(playerSelection, computerSelection){
@@ -16,22 +15,57 @@ function playRound(playerSelection, computerSelection){
   let computerChoice=computerSelection.toLowerCase();
   // finding the winner
   if(playerChoice===computerChoice){
+    result=0;
     return "its a tie"
   }else if(playerChoice=="rock" && computerChoice=="scissors"){
+    result=1;
     return `you win! rock beats scissors`
   }
   else if(playerChoice=="paper" && computerChoice=="rock"){
+    result=1;
     return `you win! paper beats rock`
   }else if(playerChoice=="scissors" && computerChoice=="paper"){
+    result=1
     return `you win! scissors beats paper`
   }else{
+    result=-1
     return `you Lose! ${computerChoice} beats ${playerChoice}`
   }
 }
+// this function is used to play the game for 5 times and announce the winner at the end 
+function game(){
+  let playerScore=0;
+  let computerScore=0;
+  let Ties=0;
 
+  for (i=0; i<5; i++){
+    const computerSelection = getComputerChoice();
+    let playerSelection=prompt("type your choice ")
+    const Rps=playRound(playerSelection, computerSelection)
+    if (result===1){
+      playerScore++;
+    }else if(result===-1){
+      computerScore++;
+    }else if(result===0){
+      Ties++
+    }else{
+      continue
+    }
+    
+  }
+  console.log(playerScore)
+  console.log(computerScore)
+  console.log(Ties)
+  if(playerScore>computerScore){
+     
+  }else if(playerScore<computerScore){
+    console.log("computer won!")
+  }else{
+    console.log("it's Tie")
+  }
+  
 
-const playerSelection = "scissors";
+}
 
-console.log(playRound(playerSelection, computerSelection));
-
+let a=game()
 
